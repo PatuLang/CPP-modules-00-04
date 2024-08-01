e536bc02c382283bc4f56892b319fe31f0c56e21
Patrik Lang <patrik.laang@gmail.com>
2024-08-01 18:46:54 +0300

    module ex02 eval ready

---
+++ module01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/01 15:08:55 by user              #+#    #+#             */
+/*   Updated: 2024/08/01 15:45:11 by user             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+    std::string brain = "HI THIS IS BRAIN";
+    std::string *stringPTR = &brain;
+    std::string &stringREF = brain;
+
+    std::cout << "The memory address of the string variable: " << &brain << std::endl;
+    std::cout << "The memory address held by stringPTR: " << &stringPTR << std::endl;
+    std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+
+    std::cout << "The value of the string variable: " << brain << std::endl;
+    std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+}
