97854c9d31e31a75e5a4de7774fb7d59765cb338
PatuLang <patrik.laang@gmail.com>
2024-08-29 18:27:54 +0300

    main with loop with prompt for user

---
+++ module01/ex06/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plang <plang@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/29 17:29:45 by plang             #+#    #+#             */
+/*   Updated: 2024/08/29 17:41:26 by plang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int	main(void)
+{
+	Harl _Harl;
+	std::string	level;
+	try
+	{
+		while (1)
+		{
+			std::cout << "Choose one: debug, info, warning, error > ";
+            std::getline(std::cin, level);
+            if (std::cin.eof())
+            {
+                std::cout << std::endl;
+                throw std::runtime_error("Harl is malfunctioning, bye bye!");
+            }
+			_Harl.complain(level);
+		}
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+}
