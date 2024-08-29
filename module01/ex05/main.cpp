e96abc062cc128f23ccb8ab92efe2e07447a8163
PatuLang <patrik.laang@gmail.com>
2024-08-29 15:45:05 +0300

    main now runs in a loop and gives prompt for the user of different complaints that Harl has to say, EOF ends the loop

--- module01/ex05/main.cpp
+++ module01/ex05/main.cpp
@@ -6,22 +6,38 @@
 /*   By: plang <plang@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 15:05:26 by plang             #+#    #+#             */
-/*   Updated: 2024/08/28 17:00:14 by plang            ###   ########.fr       */
+/*   Updated: 2024/08/29 15:13:40 by plang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int	main(int argc, char **argv)
+int	main(void)
 {
-	if (argc == 2)
+	Harl _Harl;
+	std::string	level;
+	try
 	{
-		Harl _Harl;
-		std::string	level;
-
-		level = argv[1];
-		_Harl.complain(level);
+		while (1)
+		{
+			std::cout << "Choose one: debug, info, warning, error > ";
+            std::getline(std::cin, level);
+            if (std::cin.eof())
+            {
+                std::cout << std::endl;
+                throw std::runtime_error("Harl is malfunctioning");
+            }
+			// if (argc == 2)
+			// {
+				// level = argv[1];
+				_Harl.complain(level);
+			// }
+		}
 	}
-	else
-		std::cout << "Select one of these: debug, info, warning, error" << std::endl;
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	// else
+		// std::cout << "Select one of these: debug, info, warning, error" << std::endl;
 }
