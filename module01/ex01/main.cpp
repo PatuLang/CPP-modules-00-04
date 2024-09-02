5eb2c1e8d28a213ca45c34cb5e2f91bc319f3b45
PatuLang <patrik.laang@gmail.com>
2024-09-02 17:58:33 +0300

    newHorde and theHorde for more visual understanding when running the executable

--- module01/ex01/main.cpp
+++ module01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: plang <plang@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 14:26:01 by plang             #+#    #+#             */
-/*   Updated: 2024/08/05 14:26:12 by plang            ###   ########.fr       */
+/*   Updated: 2024/09/02 17:55:49 by plang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,15 @@ int main(void)
 {
     int n = 5;
     Zombie* theHorde = zombieHorde(n, "Fred");
+    Zombie* newHorde = zombieHorde(n, "Alfred");
     for (int i = 0; i < n; i++)
     {
         theHorde[i].announce();
     }
-
     delete[] theHorde;
+    for (int i = 0; i < n; i++)
+    {
+        newHorde[i].announce();
+    }
+    delete[] newHorde;
 }
