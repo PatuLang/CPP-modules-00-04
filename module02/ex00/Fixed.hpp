ac3b0a9bff0da16dcdc60c078c97dee26c92921d
PatuLang <patrik.laang@gmail.com>
2024-09-18 17:32:08 +0300

    removed unused library

--- module02/ex00/Fixed.hpp
+++ module02/ex00/Fixed.hpp
@@ -6,14 +6,13 @@
 /*   By: plang <plang@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 12:19:31 by plang             #+#    #+#             */
-/*   Updated: 2024/09/17 17:58:09 by plang            ###   ########.fr       */
+/*   Updated: 2024/09/18 13:55:26 by plang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
-# include <string>
 # include <iostream>
 
 class Fixed
